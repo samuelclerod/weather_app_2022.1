4791878ed0ff522318ba3f55d7665ce82fb9fd4d
Samuel Rodrigues <samuelclerod@gmail.com>
2022-03-15 21:45:44 -0300

    feat: finished geocode service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const request = require('postman-request');
 const forecast = require('./services/forecast');
+const geocode = require('./services/geocode');
 
 const lati = '-7.2053646', long = "-39.3091651";
 
@@ -11,4 +12,9 @@ forecast(lati, long, (error, response) => {
   const { temperature, feelslike, weather } = response;
 
   console.log(`Actually is ${weather}, ${temperature} degrees and feels like ${feelslike}`);
+})
+
+geocode("FJN", (err, response) => {
+  if (err) return console.log(err);
+  console.log(response)
 })
--- services/geocode.js
+++ services/geocode.js
@@ -1,3 +1,79 @@
+const request = require("postman-request");
 const { GEOCODE_BASE_URL, GEOCODE_ACCESS_KEY } = require("../config");
 
-const url = `${GEOCODE_BASE_URL}/${LOCATION}.json?access_token=${GEOCODE_ACCESS_KEY}`
+// const location = 'FJN';
+
+// const url = `${GEOCODE_BASE_URL}/${location}.json?access_token=${GEOCODE_ACCESS_KEY}`
+
+// request(url, (error, _, body) => {
+
+//   if (error) {
+//     return console.log("We cannot connect to the server!");
+//   }
+
+//   const { features, message } = JSON.parse(body);
+
+//   if (message) {
+//     return console.log(message);
+//   }
+
+//   if (features.length === 0) {
+//     return console.log("Location not found!");
+//   }
+
+//   const { text, place_name, center } = features[0]
+
+//   const [longitude, latitude] = center
+
+//   const responseData = {
+//     latitude,
+//     longitude,
+//     name: text,
+//     address: place_name,
+//   }
+
+// })
+
+
+const geocode = (searchTerm, callback) => {
+
+  const url = `${GEOCODE_BASE_URL}/${searchTerm
+    }.json?access_token=${GEOCODE_ACCESS_KEY}`
+
+  request(url, (error, _, body) => {
+
+    if (error) {
+      return callback(
+        "We cannot connect to the server!",
+        undefined
+      )
+    }
+
+    const { features, message } = JSON.parse(body);
+
+    if (message) {
+      return callback(message, undefined);
+    }
+
+    if (features.length === 0) {
+      return callback("Location not found!", undefined);
+    }
+
+    const { text, place_name, center } = features[0];
+
+    const [longitude, latitude] = center
+
+    const responseData = {
+      latitude,
+      longitude,
+      name: text,
+      address: place_name,
+    }
+
+    callback(undefined, responseData);
+
+  })
+}
+
+
+module.exports = geocode
