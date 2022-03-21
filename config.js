0831f01c998c6ba677932e6863f314c193f8e371
Samuel Rodrigues <samuelclerod@gmail.com>
2022-03-21 18:43:46 -0300

    fix: remover code smels

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
   FORECAST_BASE_URL: 'http://api.weatherstack.com/current',
-  FORECAST_ACCESS_KEY: 'adc237cd619def2e5e8cf3ed7c8ef77e',
+  FORECAST_ACCESS_KEY: '',
   GEOCODE_BASE_URL: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
-  GEOCODE_ACCESS_KEY: 'pk.eyJ1Ijoic2FtdWVsY2xlcm9kIiwiYSI6ImNsMHNxOHh4dTAzZ2ozanBhNGQ2NG95MDUifQ.bcKnKsYjXv4PQCi4UaiZFA'
+  GEOCODE_ACCESS_KEY: ''
 }
 
 module.exports = config;
