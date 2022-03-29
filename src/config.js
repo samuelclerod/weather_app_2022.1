2f5d8c254d5e5315cc1232f5fb96d3cdb52bd358
Samuel Rodrigues <samuelclerod@gmail.com>
2022-03-29 20:02:44 -0300

    fix: add config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  FORECAST_BASE_URL: process.env.FORECAST_BASE_URL,
+  FORECAST_ACCESS_KEY: process.env.FORECAST_ACCESS_KEY,
+  GEOCODE_BASE_URL: process.env.GEOCODE_BASE_URL,
+  GEOCODE_ACCESS_KEY: process.env.GEOCODE_ACCESS_KEY
+}
+
+module.exports = config;
